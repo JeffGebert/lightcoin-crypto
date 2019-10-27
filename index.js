bf49a3b09dac3c2879acb0f0f9671be7d32d7136
Jeff Gebert <jeffgebert@hotmail.com>
2019-10-27 18:54:46 +0000

    added limit on withrawls

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
+
+
+  }
+};
+
+
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+
+
+}
+
+class Deposit extends Transaction {
+
+
+  get value() {
+    return this.amount
+  }
+
+  isAllowed() {
+    // deposits always allowed thanks to capitalism.
+    return true;
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let sum = 0;
+    this.transactions.forEach(element => {
+      sum += element.value;
+    })
+    return sum;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
@@ -18,12 +75,16 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+console.log("Starting Balance $", myAccount.balance);
+
+const t1 = new Deposit(100, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+
+const t2 = new Withdrawal(120, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+console.log('Ending Balance:', myAccount.balance);
+
